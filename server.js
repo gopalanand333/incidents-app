f0681b574a6b2d2c3a6bbbdfcb22f6082ffa2d29
I807787 <yevgen.trukhin@sap.com>
2025-10-21 08:39:23 -0400

    Clean db

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ cds.serve('all').in(app).then(() => {
     app.listen()
 })
 
-cds.on("served", async () => {
+/*cds.on("served", async () => {
     try {
         const { DELETE } = cds.ql;
         await cds.run(DELETE.from("sap.capire.incidents.Incidents").where({title: 'test123'}));
@@ -39,6 +39,6 @@ cds.on("served", async () => {
     } catch (error) {
         console.error("Failed to clean database:", error);
     }
-});
+});*/
 
 module.exports = cds.server
